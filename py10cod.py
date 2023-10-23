b71f5b9f8248d2b858cb458370d2952a8e41658a
Aliah Iqbal <93481483+php20ai@users.noreply.github.com>
2023-10-23 08:21:14 +0100

    Update py10cod.py

--- py10cod.py
+++ py10cod.py
@@ -16,13 +16,15 @@ print(solution([3, 8, 9, 7, 6], 3 ))
 
 #### solution to codility problem 2 #####
 
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
 
-# All comments shown below are from the applicant
-# First, I notice a pattern when I see examples from task 1, i.e., when A=[3,8,9,7,6] and K=3, the "solution" function returns [9,7,6,3,8], here I notice that the value "3" in array A has index 0, and "3" in [9,7,6,3,8] has index 3, 8 in array A has index value 1 and 8 in [9,7,6,3,8] has index value 4, and so on, this also applies to all other examples in task 1. So I notice that get the out put of the function "solution" from its input array, we need to add K to each index in A, then take modulo N, where N is the length of array A, then the value at the original index i in A, now has index at K+i in the output array, so e.g.
-
-# consider A=[3,8,,97,6,3,8] and K=3, in A "9" has index 0, so 0+K=0+3=3, and 3mod(5)=3, where 5 is length of array A, so "9" is now at index 3 in the output array []
+# First, I notice a pattern when I see examples from task 1, i.e., when A=[3,8,9,7,6] and K=3, the "solution" function returns [9,7,6,3,8], here I notice that the value "3" in array A has index 0, 
+# and "3" in [9,7,6,3,8] has index 3, 8 in array A has index value 1 and 8 in [9,7,6,3,8] has index value 4, and so on, 
+# this applies to all other examples in task 1. 
+# So, to get the output of the function from its input array,
+# we need to add K to each index in A, then take modulo N,
+# where N is the length of array A, then the element of the input array A which had index i, now has index K+i in the output array, for e.g.
+# consider A=[3,8,9,7,6] and K=3, in the array A the element "3" has index 0, so 0+K=0+3=3, and 3mod(5)=3, where 5 is the length of array A, 
+# so now the element "3" has index 3 in the output array.
 
 def solution(A, K):
    N=len(A) # here N is the length of rray A
